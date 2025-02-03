69072fee6551bd737af147800530266c00248cf4
federico-arona <federico.arona.94@gmail.com>
2025-02-04 00:54:41 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-   base: "/spinners-website/"
-})
+  base: "/spinners-website/",
+  build: {
+    terserOptions: {
+      compress: {
+        unsafe: true, // Reduce the use of `eval()`
+      }
+    }
+  }
+});
