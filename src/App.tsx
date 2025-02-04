36fc07c7db8b33af106471b3b20f7f487ef1a3b5
federico-arona <federico.arona.94@gmail.com>
2025-02-04 11:14:48 +0100

    change router to work on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Home from './components/organisms/Home';
 import ProjectDetails from './components/organisms/ProjectDetails';
 
 function App() {
   return (
-    <>
-     <Router>
+    <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:id" element={<ProjectDetails />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<ProjectDetails />} />
       </Routes>
     </Router>
-    </>
   );
 }
 
