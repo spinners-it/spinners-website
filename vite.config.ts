b3327857388a70a24a9d92858862441095fe2385
federico-arona <federico.arona.94@gmail.com>
2025-02-04 11:11:00 +0100

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/spinners-website/",
-  build: {
-    terserOptions: {
-      compress: {
-        unsafe: true,
-      }
-    }
-  }
 });
